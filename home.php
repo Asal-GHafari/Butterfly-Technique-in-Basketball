da673507a4727b3c4d497c7915b9f477d6e19475
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-07-05 16:40:39 +0330

    change

--- home.php
+++ home.php
@@ -3,23 +3,19 @@
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
+<script type="text/javascript" src="jquery/jquery.js"></script>
 <link  type="text/css" rel="stylesheet" href="css.css" />
+<script type="text/javascript" src="jquery/animate.js"></script>
 </head>
 
 <body>
 <div class="content">
-<div class="top-left"></div>
-<div class="top-left"></div>
-<div class="top-left" id="top"></div>
-<div class="top-rigth"></div>
-<div class="top-rigth"></div>
-<div class="bottom-left"></div>
-<div class="bottom-left" id="top"></div>
-<div class="bottom-rigth" id="top-rigth"></div>
-<div class="bottom-rigth"></div>
-
-
-
+    <div class="top-left1"  id="bazikon"></div>
+    <div class="top-left2"  id="bazikon"></div>
+    <div class="top-rigth1" id="bazikon"></div>
+    <div class="top-rigth2" id="bazikon"></div>
+    <div class="bottom-left" id="bazikon"></div>
+    <div class="bottom-rigth" id="bazikon"></div>
 </div>
 </body>
 </html>
---
+++ jquery/animate.js
@@ -0,0 +1,13 @@
+$(function(){
+$("div.top-left:nth-child(2)").click(function(){
+	r=$("div#top1").css({"position":"absolute","margin-left":"400px"});
+	//alert(r);
+
+});
+
+
+
+
+
+
+});
