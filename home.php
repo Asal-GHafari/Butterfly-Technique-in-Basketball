cd06b721d29ffb52bce94eb74073f657023899c4
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-07-05 11:18:52 +0330

    extend

--- home.php
+++ home.php
@@ -12,9 +12,9 @@
 <div class="top-left"></div>
 <div class="top-left" id="top"></div>
 <div class="top-rigth"></div>
-<div class="top-rigth"></div><div class="top-left" id="top"></div>
-<div class="bottom-left"></div><div class="top-left" id="top"></div>
-<div class="bottom-rigth"></div><div class="top-left" id="top"></div>
+<div class="top-rigth"></div>
+<div class="bottom-left"></div>
+<div class="bottom-rigth"></div>
 
 
 </div>
