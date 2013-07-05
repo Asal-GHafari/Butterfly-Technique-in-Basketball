6adfce4c0e91427e1ee7286143b1c11ae2d1f8e3
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-07-05 10:43:49 +0330

    Initialize

---
+++ home.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link  type="text/css" rel="stylesheet" href="css.css" />
+</head>
+
+<body>
+<div class="content">
+<div class="top-left"></div>
+<div class="top-left"></div>
+<div class="top-left" id="top"></div>
+<div class="top-rigth"></div>
+<div class="top-rigth"></div><div class="top-left" id="top"></div>
+<div class="bottom-left"></div><div class="top-left" id="top"></div>
+<div class="bottom-rigth"></div><div class="top-left" id="top"></div>
+
+
+</div>
+</body>
+</html>
